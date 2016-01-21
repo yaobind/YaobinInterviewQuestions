0b98748860139330d9b7c3e578eb843592c612f2
yaobind <yaobin.dong@gmail.com>
2016-01-21 14:38:47 -0500

    finished the checkClean() method and minor change

--- interview qustions/src/CounterImpl.java
+++ interview qustions/src/CounterImpl.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /*
 interface counter {
@@ -19,12 +20,26 @@ need to do both ArrayList version and LinkedList version  */
 
 
 public class CounterImpl implements Counter{
-	private ArrayList<Date> log;
+	private List<Date> log;
+	private int limit;
+	
+	public int getLimit() {
+		return limit;
+	}
+
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+
 	public CounterImpl(){
 		this.log = new ArrayList<Date>();
 	}
 	
-	public CounterImpl(ArrayList<Date> log){
+	public List<Date> getLog() {
+		return log;
+	}
+
+	public void setLog(List<Date> log) {
 		this.log = log;
 	}
 
@@ -34,31 +49,38 @@ public class CounterImpl implements Counter{
 	}
 
 	private void checkClean(){
-	//todo: check if need to clean and clean log
+		if(this.log.size() > this.limit){
+			List<Date> newLog =  this.log.subList(findFirstEntryIndex(getLastMin(), this.log, 0, this.log.size()-1), this.log.size()-1);
+			this.log = newLog;
+		}
+	}
+	
+	private Date getLastMin(){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.MINUTE, -1);
+		return cal.getTime();
 	}
 
 	public int countLastMin(){
 		//need to count the entries in last min, but no need to count one by one, use the array’s index is great
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(new Date());
-		cal.add(Calendar.MINUTE, -1);
-		Date lastMin = cal.getTime();
+
 		//do binary search to find the first entry in last min
-		return this.log.size() - findFirstEntryIndex(lastMin, log, 0,  log.size());	
+		return this.log.size() - findFirstEntryIndex(getLastMin(), log, 0,  log.size()-1);	
 	}
 
-	private int findFirstEntryIndex (Date lastMin, ArrayList<Date> log, int start, int end){
-		if(start == end )
-			return start;
-		int mid = (start + end)/2;
+	private int findFirstEntryIndex (Date lastMin, List<Date> log, int startIndex, int endIndex){
+		if(startIndex == endIndex )
+			return startIndex;
+		int mid = (startIndex + endIndex)/2;
 	
 		//assumption: there is no duplicates in the arrayList 
 		if(lastMin.equals(log.get(mid))){
 			return mid;
 		} else if(lastMin.before(log.get(mid))){
-			return findFirstEntryIndex (lastMin, log, start, mid );
+			return findFirstEntryIndex (lastMin, log, startIndex, mid );
 		} else {
-			return findFirstEntryIndex (lastMin, log, mid+1, end );
+			return findFirstEntryIndex (lastMin, log, mid+1, endIndex );
 		}
 	}
 }
--- interview qustions/src/CounterImplTest.java
+++ interview qustions/src/CounterImplTest.java
@@ -10,6 +10,7 @@ public class CounterImplTest {
 	@Test
 	public void test() {
 		ArrayList<Date> log = new ArrayList<Date>();
+		
 		Calendar cal = Calendar.getInstance();
 
 		cal.setTime(new Date());
@@ -45,8 +46,15 @@ public class CounterImplTest {
 		cal.add(Calendar.SECOND, 5);
 		log.add(cal.getTime());
 		
-		Counter ct = new CounterImpl(log);
+		CounterImpl ct = new CounterImpl();
+		ct.setLog(log);
+		ct.setLimit(10);
+		assertEquals(13, ct.getLog().size());
 		assertEquals(7, ct.countLastMin());
+		ct.inc();
+		assertEquals(7, ct.getLog().size());
+		assertEquals(7, ct.countLastMin());
+		
 	}
 
 }
